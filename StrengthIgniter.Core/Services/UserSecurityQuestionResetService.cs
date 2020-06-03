afc1697ffc1fc85dbffd646519f570aff8972161
alwynthomas.dev <me@alwynthomas.dev>
2020-06-03 10:59:09 +0100

    WIP

---
+++ StrengthIgniter.Core/Services/UserSecurityQuestionResetService.cs
@@ -0,0 +1,23 @@
+﻿using StrengthIgniter.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StrengthIgniter.Core.Services
+{
+    public interface IUserSecurityQuestionResetService
+    {
+        IEnumerable<UserSecurityQuestionAnswerModel> GetFailedQuestionsForUser(Guid userReference);
+        void UpdateSecretQuestions
+
+        /*
+         * TODO: 
+         *  Get questions that need resetting for user
+         *  Reset questions
+         */
+    }
+
+    public class UserSecurityQuestionResetService
+    {
+    }
+}
